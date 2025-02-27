e7bbdfa28eea983962eb4ef8310b73961419b389
Prabin-ai27 <prabinkumarpradhan1205@gmail.com>
2025-02-27 18:20:29 +0530

    fixt every think

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// server.js - Entry point for Node.js backend
+
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
@@ -38,5 +38,7 @@ mongoose.connect(process.env.MONGO_URI, {
 }).then(() => console.log('MongoDB Connected')).catch(err => console.error('MongoDB connection error:', err));
 
 // Start Server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`)); 
+
+module.exports = app;
