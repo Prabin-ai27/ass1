432a6c518f0090c3b0406d440c180554e1ede10a
Prabin-ai27 <prabinkumarpradhan1205@gmail.com>
2025-02-27 18:01:28 +0530

    six update

--- server.js
+++ server.js
@@ -40,4 +40,3 @@ mongoose.connect(process.env.MONGO_URI, {
 // Start Server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-module.exports = app;
