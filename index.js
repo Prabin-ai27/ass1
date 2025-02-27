de6ce5d88f2c389c016662e1cc5a36bd1993d5d0
Prabin-ai27 <prabinkumarpradhan1205@gmail.com>
2025-02-27 19:12:14 +0530

    cnscnsc

--- index.js
+++ index.js
@@ -39,6 +39,6 @@ mongoose.connect(process.env.MONGO_URI, {
 
 // Start Server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT,'0,0,0,0', () => console.log(`Server running on port ${PORT}`)); 
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`)); 
 
 // module.exports = app;
